8608d244ed968a28d629553f1d7a9929d31c64b5
David Baca <116526025+davidjbaca@users.noreply.github.com>
2023-04-12 13:45:10 -0500

    finished css and added api key

---
+++ index.js
@@ -0,0 +1,8 @@
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+let getMovie = () =>{
+    let movieName = movieNameRef.value;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+}
